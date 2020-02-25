1dc2f3f65c06669dcdaea544ad51c0d0200192a9
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2020-02-25 11:10:58 +0100

    Fixed failing http tests

--- src/test/java/no/ssb/dapla/data/access/service/DataAccessServiceHttpTest.java
+++ src/test/java/no/ssb/dapla/data/access/service/DataAccessServiceHttpTest.java
@@ -1,6 +1,10 @@
 package no.ssb.dapla.data.access.service;
 
+import no.ssb.dapla.data.access.protobuf.AccessTokenRequest;
 import no.ssb.dapla.data.access.protobuf.AccessTokenResponse;
+import no.ssb.dapla.data.access.protobuf.LocationRequest;
+import no.ssb.dapla.data.access.protobuf.LocationResponse;
+import no.ssb.testing.helidon.GrpcMockRegistryConfig;
 import no.ssb.testing.helidon.IntegrationTestExtension;
 import no.ssb.testing.helidon.TestClient;
 import org.junit.jupiter.api.Test;
@@ -8,7 +12,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import javax.inject.Inject;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 @ExtendWith(IntegrationTestExtension.class)
+@GrpcMockRegistryConfig(DataAccessServiceGrpcTest.DataAccessGrpcMockRegistry.class)
 class DataAccessServiceHttpTest {
 
     @Inject
@@ -16,7 +24,30 @@ class DataAccessServiceHttpTest {
 
     @Test
     void thatGetAccessTokenWorks() {
-        testClient.get("/access/token?userId=user&location=myLocation&privilege=READ", AccessTokenResponse.class).expect200Ok().body();
+        AccessTokenRequest accessTokenRequest = AccessTokenRequest.newBuilder()
+                .setUserId("user")
+                .setPath("myLocation")
+                .setPrivilege(AccessTokenRequest.Privilege.READ)
+                .build();
+        AccessTokenResponse response = testClient.post("/rpc/DataAccessService/getAccessToken", accessTokenRequest,
+                AccessTokenResponse.class).body();
+        assertNotNull(response);
+        assertThat(response.getAccessToken()).isEqualTo("localstack-token");
+        assertThat(response.getExpirationTime()).isGreaterThan(System.currentTimeMillis());
     }
 
+    @Test
+    public void thatGetLocationWorks() {
+        LocationRequest locationRequest = LocationRequest.newBuilder()
+                .setUserId("user")
+                .setSnapshot(2)
+                .setPath("/path/to/dataset")
+                .build();
+
+        LocationResponse response = testClient.post("/rpc/DataAccessService/getLocation", locationRequest,
+                LocationResponse.class).body();
+        assertNotNull(response);
+        assertThat(response.getParentUri()).isEqualTo("gs://root");
+        assertThat(response.getVersion()).isEqualTo("1");
+    }
 }
