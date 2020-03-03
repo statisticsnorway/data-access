ebacb5d8221be1a656ba71954382771bc437534c
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-03-03 07:11:35 +0100

    Remove unused module requirement

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -48,7 +48,6 @@ module no.ssb.dapla.data.access {
     requires io.helidon.tracing;
     requires com.google.auth.oauth2;
     requires com.google.auth;
-    requires no.ssb.dapla.dataset.uri;
 
     provides no.ssb.helidon.application.HelidonApplicationBuilder with DataAccessApplicationBuilder;
 }
