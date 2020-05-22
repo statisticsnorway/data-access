1c6518baa0f0fb094b4906db72e44a3849f79f1a
Øyvind Strømmen <insula.ventus@gmail.com>
2020-05-22 12:45:23 +0200

    Default to "" if missing property 'target.uri.path-prefix'

--- src/main/java/no/ssb/dapla/data/access/service/Route.java
+++ src/main/java/no/ssb/dapla/data/access/service/Route.java
@@ -15,7 +15,7 @@ public class Route {
         this.uri = config.get("target").get("uri").as(uri ->
                 URI.create(uri.get("scheme").asString().get() + "://" +
                         uri.get("host").asString().get() +
-                        uri.get("path-prefix").asString().get()
+                        uri.get("path-prefix").asString().orElse("")
                 )).get();
         this.auth = config.get("target").get("auth").detach().asMap().orElse(new HashMap<>());
     }
